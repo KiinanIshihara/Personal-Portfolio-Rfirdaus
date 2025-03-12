841ea7ef76cc16df8637f78176a4bd134c834ac1
Keenan <97200272+KiinanIshihara@users.noreply.github.com>
2025-03-12 02:24:51 -0500

    added template banner, skills section, and project section (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -8,6 +11,10 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+
     </div>
   );
 }
